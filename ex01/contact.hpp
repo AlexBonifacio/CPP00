77def73fd886f1cc6f2219f8184e6a68d204203c
alexlinux <alex13.bonifacio@gmail.com>
2025-10-22 13:24:11 +0200

    CPP00 ex01 begin

---
+++ ex01/contact.hpp
@@ -0,0 +1,17 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+class Contact
+{
+	public:
+		Contact();
+		~Contact();
+
+	private:
+	std::string	_firstName;
+	std::string _lastName;
+	std::string _
+
+};
+
+#endif CONTACT_HPP
---
+++ ex01/main.cpp
@@ -0,0 +1,6 @@
+#include "mapb.hpp"
+
+int	main(void)
+{
+	return 0;
+}
